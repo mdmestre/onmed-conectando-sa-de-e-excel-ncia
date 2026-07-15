2cbe30ff9a49e03ac7eb019d84764eea5ab8abcb
ppv <paulofn49@gmail.com>
2026-07-15 12:53:21 -0300

    Set correct GitHub Pages base path for current repo

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/onmed-conectando-sa-de-e-excel-ncia/",
+  base: mode === "development" ? "/" : "/Paulo.Site-Onmedic/",
   server: {
     host: "::",
     port: 8080,
